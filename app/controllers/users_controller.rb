97fb0e8886def84741ae6298743ef48e4fa24401
wbinnovation <info@wbinnova.com>
2017-06-03 14:07:55 -0400

    Se completo la funcionalidad de CREAR usuarios por la direccion /users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		@user.save
+		redirect_to @user
 	end
 
 	def destroy
@@ -14,4 +17,14 @@ class UsersController < ApplicationController
 	def show
 	end
 
+	private
+		def user_params
+			params.require(:user).permit(
+				:rut,
+				:name,
+				:lasName,
+				:birthDate,
+				:email,
+				:password)
+		end
 end
