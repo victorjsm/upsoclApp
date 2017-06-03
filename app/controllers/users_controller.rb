9ce106882ffb04e6824a87394a7b5034e720bf75
wbinnovation <info@wbinnova.com>
2017-06-03 14:32:02 -0400

    Se completo la funcionalidad de Actualizar la informacion de usuarios y se coloco un link para acceder a esta, en la vista index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,28 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		@user.save
-		redirect_to @user
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
 
 	def destroy
 	end
 
 	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
 	end
 
 	def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	validates :name,:lastName,:email,:password,:birthDate,:rut, presence: true
+
 end
